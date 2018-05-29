27b366ad4bff02ba82a7911b31ca9d9e5d22c4bc
Punith13 <puneethu@gmail.com>
2018-05-29 22:17:49 +1000

    Travis configuration

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
 	googleClientID: '718256738752-blihkr4smrq4519ojj79p73q1pn875ps.apps.googleusercontent.com',
 	googleClientSecret: 'ws8FqAjvSfI8N70UlwuPz9C_',
-	mongoURI: 'mongodb://punith:password@ds123752.mlab.com:23752/emaily-dev007',
+	mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
 	cookieKey: '12ewrefweffdsds3123123',
 	redisUrl: 'redis://127.0.0.1:6379'
 };
